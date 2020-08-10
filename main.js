7e8c36c4fb67179d05abf91dbd869a36d252ae5a
amylinda <66226055+amylinda@users.noreply.github.com>
2020-08-09 19:40:33 -0500

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.header_toggleBtn');
-const menu = document.querySelector('.header_menu');
-
-toggleBtn.addEventListener('click', ()=>{
-menu.classList.toggle('active');
-});
